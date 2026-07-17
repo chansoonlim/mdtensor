5b496e159bbc906f60d2d940d68bee4392756bc8
chansoonlim <49858020+chansoonlim@users.noreply.github.com>
2026-07-17 22:25:50 +0000

    Update single header build.

--- mdtensor.hpp
+++ mdtensor.hpp
@@ -97,6 +97,8 @@ namespace std::experimental {
  */
 
 
+#include <concepts>
+
 #ifndef MDSPAN_SINGLE_HEADER_INCLUDE_GUARD_ // NOTE: for godbolt test
 #include <experimental/mdarray>
 #include <experimental/mdspan> // TODO: Remove when C++23 std::mdspan supports
@@ -1548,6 +1550,9 @@ template <typename data_t = void, arithmetic_c stop_t>
 [[nodiscard]] inline constexpr auto arange(stop_t &&stop) noexcept {
     return arange<data_t>(0, std::forward<stop_t>(stop));
 }
+
+// TODO: Develop arange that works in compile-time contexts (e.g., constexpr
+// mdarray) and/or with static extents.
 
 } // namespace mdtensor
 //END_FILE_INCLUDE: /home/runner/work/mdtensor/mdtensor/mdtensor/creation/arange.hpp
@@ -4941,6 +4946,75 @@ template <int64_t Axis, typename... ins_t>
 
 } // namespace mdtensor
 //END_FILE_INCLUDE: /home/runner/work/mdtensor/mdtensor/mdtensor/manipulation/concatenate.hpp
+//BEGIN_FILE_INCLUDE: /home/runner/work/mdtensor/mdtensor/mdtensor/manipulation/transpose.hpp
+/**
+ * @file
+ * @brief Transpose utilities for mdtensor.
+ *
+ * @copyright
+ * SPDX-License-Identifier: Apache-2.0
+ * See README and LICENSE files for full attribution details.
+ */
+
+
+#include <array>
+#include <type_traits>
+#include <utility>
+
+
+namespace mdtensor {
+
+template <typename in_t, std::integral AxesType, AxesType... Axes>
+[[nodiscard]] inline constexpr auto
+transpose(in_t &&in, std::integer_sequence<AxesType, Axes...>) noexcept {
+    const auto in_mds = core::to_mdspan(std::forward<in_t>(in));
+    using in_mds_t = decltype(in_mds);
+
+    constexpr size_t rank = in_mds_t::rank();
+
+    static_assert(sizeof...(Axes) == rank, "Number of axes must match rank.");
+
+    if constexpr (rank == 0 || rank == 1) {
+        return in_mds;
+
+    } else {
+        constexpr auto axes = std::array<size_t, rank>{static_cast<size_t>(
+            ((Axes % static_cast<AxesType>(rank)) + (rank)) % rank)...};
+
+        const auto new_extents = [&]<size_t... Is>(std::index_sequence<Is...>) {
+            return extents<typename in_mds_t::index_type,
+                           in_mds_t::static_extent(axes[Is])...>{
+                in_mds.extent(axes[Is])...};
+        }(std::make_index_sequence<rank>{});
+
+        const auto new_strides = [&]<size_t... Is>(std::index_sequence<Is...>) {
+            return std::array<typename in_mds_t::index_type, rank>{
+                in_mds.stride(axes[Is])...};
+        }(std::make_index_sequence<rank>{});
+
+        return mdspan<typename in_mds_t::element_type,
+                      std::remove_cvref_t<decltype(new_extents)>, layout_stride,
+                      typename in_mds_t::accessor_type>{
+            in_mds.data_handle(),
+            layout_stride::mapping{new_extents, new_strides}};
+    }
+}
+
+template <typename in_t>
+[[nodiscard]] inline constexpr auto transpose(in_t &&in) noexcept {
+    const auto in_mds = core::to_mdspan(std::forward<in_t>(in));
+    using in_mds_t = decltype(in_mds);
+
+    constexpr size_t rank = in_mds_t::rank();
+
+    return [&]<size_t... Is>(std::index_sequence<Is...>) {
+        return transpose(std::forward<in_t>(in),
+                         std::integer_sequence<int64_t, rank - 1 - Is...>{});
+    }(std::make_index_sequence<rank>{});
+}
+
+} // namespace mdtensor
+//END_FILE_INCLUDE: /home/runner/work/mdtensor/mdtensor/mdtensor/manipulation/transpose.hpp
 //END_FILE_INCLUDE: /home/runner/work/mdtensor/mdtensor/mdtensor/manipulation/manipulation.hpp
 //BEGIN_FILE_INCLUDE: /home/runner/work/mdtensor/mdtensor/mdtensor/math/math.hpp
 /**
