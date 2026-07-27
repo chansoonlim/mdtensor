79ccecd396ff775d7ee42272b1c12bc3135a6d8e
Chan-Soon Lim <chansoon0623@gmail.com>
2026-07-27 22:05:37 +0000

    feat(broadcast): enhance broadcasting assertions and stride calculations
    - Add runtime assertions to validate broadcasting extents.
    - Introduce a helper function to calculate new indices for dimensions.
    - Update stride calculation logic to accommodate new broadcasting rules.
    - Ensure compatibility checks for static extents during compilation.

--- mdtensor/manipulation/broadcast_to.hpp
+++ mdtensor/manipulation/broadcast_to.hpp
@@ -49,16 +49,39 @@ broadcast_to(in_t &&in,
     } else {
         using index_t = typename new_extents_base_t::index_type;
 
+        // ni = new_rank - org_rank + oi
+        const auto get_ni = [](size_t i) { return new_rank - org_rank + i; };
+
+        // assertion
+        static_assert(
+            [&] {
+                for (size_t i = 0; i < org_rank; i++) {
+                    if (in_mds_base_t::static_extent(i) !=
+                            new_extents_base_t::static_extent(get_ni(i)) &&
+                        in_mds_base_t::static_extent(i) != 1 &&
+                        new_extents_base_t::static_extent(get_ni(i)) != dyn) {
+                        return false;
+                    }
+                }
+                return true;
+            }(),
+            "Incompatible extents for broadcasting.");
+
+        for (size_t i = 0; i < org_rank; i++) {
+            assert(static_cast<size_t>(in_mds.extent(i)) ==
+                       static_cast<size_t>(new_extents.extent(get_ni(i))) ||
+                   static_cast<size_t>(in_mds.extent(i)) == 1);
+        }
+
+        // calculation
         auto new_strides = std::array<index_t, new_rank>{};
 
-        for (size_t i = 0; i < new_rank; i++) {
-            if (i < new_rank - org_rank) {
-                new_strides[i] = 0;
+        for (size_t i = 0; i < new_rank - org_rank; i++) {
+            new_strides[i] = 0;
+        }
 
-            } else {
-                new_strides[i] = static_cast<index_t>(
-                    in_mds.stride(i - (new_rank - org_rank)));
-            }
+        for (size_t i = 0; i < org_rank; i++) {
+            new_strides[get_ni(i)] = static_cast<index_t>(in_mds.stride(i));
         }
 
         return mdspan<typename in_mds_base_t::element_type, new_extents_base_t,
